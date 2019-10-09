fd802134d733dba7aaeb10361ecdb6fb5e4934c6
Y-Yoshimoto <mewyoshimoto@gmail.com>
2019-10-09 09:46:24 +0900

    修正

--- ITMS_Docker/apatchphp_custom/contents/Dashboard/incidentInfoAPI.php
+++ ITMS_Docker/apatchphp_custom/contents/Dashboard/incidentInfoAPI.php
@@ -1,82 +1,48 @@
 <?php
+    //incidentInfoAPI
     header("Content-Type: application/json; charset=UTF-8");
     //POST縺輔ｌ縺溷､縺ｮ蜿門ｾ
-    $incidentName = filter_input(INPUT_POST, 'incidentName');
-    $servce = filter_input(INPUT_POST, 'servce');
-    $severity = filter_input(INPUT_POST, 'severity');
-    $brief = filter_input(INPUT_POST, 'brief');
-    $handling = filter_input(INPUT_POST, 'handling');
-    $remark = filter_input(INPUT_POST, 'remark');
-    $incidenInfo=[$incidentName, $servce, $severity, $brief, $handling, $remark];
-    //繧､繝ｳ繧ｷ繝�Φ繝育匳骭ｲ
+    $incidentNumber = filter_input(INPUT_POST, 'incidentNumber');
+    error_log("$incidentNumber");
     //json繧ｪ繝悶ず繧ｧ繧ｯ繝医お繝ｳ繧ｳ繝ｼ繝,騾∽ｿ｡
-    echo json_encode(registrationIncident($incidenInfo));
+    echo json_encode(getIncidentInfo($incidentNumber));
     exit;
 ?>
 <?php
-    //繧､繝ｳ繧ｷ繝�Φ繝育匳骭ｲ
-    function registrationIncident($incidenInfo){
+    //繧､繝ｳ繧ｷ繝�Φ繝医Μ繧ｹ繝亥叙蠕
+    function getIncidentInfo($incidentNumber){
     //DB蝠上＞蜷医ｏ縺
-    $statusCode = calldatabase($incidenInfo);
-    error_log("StatusCode:"."$statusCode");
+    $incidentInfo = calldatabase($incidentNumber);
+
     //騾∽ｿ｡繝��繧ｿ逕滓�,繝ｪ繧ｿ繝ｼ繝ｳ
-    return makeInfo($statusCode);
+    //return makeInfo($incidentList);
+    return $incidentInfo;
     }
-    //繝ｬ繧ｹ繝昴Φ繧ｹ逕ｨJSON逕滓�
-    function makeInfo($statusCode){
+    /*繝ｬ繧ｹ繝昴Φ繧ｹ逕ｨJSON逕滓�
+    function makeInfo($userID, $statusCode){
         $result = array(
+        "user" => $userID,
         "statusCode" => $statusCode) ;
         return $result;
-    }
+    }*/
 ?>
-
 <?php
-    //DB逋ｻ骭ｲ
-    function calldatabase($iInfo){
+    //繧､繝ｳ繧ｷ繝�Φ繝域ュ蝣ｱ蜿門ｾ
+    function calldatabase($incidentNumber){
         //DB謗･邯
         require_once("../../inc/inquiry.inc");
-        //繧､繝ｳ繝�ャ繧ｯ繧ｹ險育ｮ
-        $incidentNumber = 1;
-        $sql = "SELECT max(incidentNumber) FROM t_incidentdata;";
+        //SQL逕滓�
+        $sql = "SELECT * FROM t_incidentdata WHERE incidentNumber = ?;";
         error_log("$sql");
-        $rst = $dbh->query($sql);
-        if($row = $rst->fetch(PDO::FETCH_NUM)){
-            $incidentNumber =  $row[0] + 1;
-        }
-
-        //繧､繝ｳ繧ｷ繝�Φ繝育匳骭ｲ
-        $nowTime = date("Y-m-d H:i:s");
-        $sql = "INSERT INTO t_incidentdata VALUES (".$incidentNumber.",'".$iInfo[1]."', 1, '".$iInfo[2]."','".$iInfo[0]."','".$iInfo[3]."','".$iInfo[4]."','".$iInfo[5]."','$nowTime','$nowTime', 0);";
-        error_log("$sql");
+        $prepare = $dbh->prepare($sql);
+        $prepare->bindValue(1, $incidentNumber, PDO::PARAM_STR);
         //SQL逋ｺ陦
-        $insert = $dbh->exec($sql);
+        $prepare->execute();
         //蜃ｺ蜉帷ｵ先棡遒ｺ隱
-        error_log("$insert"-1);
-        return "$insert"-1;
+        while($row = $prepare->fetch(PDO::FETCH_ASSOC)){
+            return $row;
+            //error_log($row['incidentNumber']."  ".$row['incidentName']."  ".$row['severity']);
+        }
+        return "1";
     }
  ?>
-
-
-<!--
- $sql = 'INSERT INTO t_incidentdata VALUES (6,?,1,?,?,?,?,?,?,?,0);';
- error_log("$sql");
- error_log("$iInfo[0]");
- error_log("$iInfo[1]");
- error_log("$iInfo[2]");
- error_log("$iInfo[3]");
- error_log("$iInfo[4]");
- error_log("$iInfo[5]");
- $prepare = $dbh->prepare($sql);
-
- $prepare->bindValue(1, $iInfo[1], PDO::PARAM_STR);
- $prepare->bindValue(2, $iInfo[2], PDO::PARAM_STR);
- $prepare->bindValue(3, $iInfo[3], PDO::PARAM_STR);
- $prepare->bindValue(4, $iInfo[0], PDO::PARAM_STR);
- $prepare->bindValue(5, $iInfo[4], PDO::PARAM_STR);
- $prepare->bindValue(6, $iInfo[5], PDO::PARAM_STR);
- $nowTime = date("Y-m-d H:i:s");
- $prepare->bindValue(7, $nowTime, PDO::PARAM_STR);
- $prepare->bindValue(8, $nowTime, PDO::PARAM_STR);
- error_log($nowTime);
- error_log("$sql");
--->
